67b31b35bac8976ebd71ac192bdc39f5e730594f
Jon. Belanger <jbelanger1@rochester.rr.com>
2017-05-10 18:03:29 -0400

    Started a common definitions file.

---
+++ src/AXP_Utility.h
@@ -0,0 +1,43 @@
+/*
+ * Copyright (C) Jonathan D. Belanger 2017.
+ * All Rights Reserved.
+ *
+ * This software is furnished under a license and may be used and copied only
+ * in accordance with the terms of such license and with the inclusion of the
+ * above copyright notice.  This software or any other copies thereof may not
+ * be provided or otherwise made available to any other person.  No title to
+ * and ownership of the software is hereby transferred.
+ *
+ * The information in this software is subject to change without notice and
+ * should not be construed as a commitment by the author or co-authors.
+ *
+ * The author and any co-authors assume no responsibility for the use or
+ * reliability of this software.
+ *
+ * Description:
+ *
+ *	This header file contains useful definitions to be used throughout the
+ *	Digital Alpha AXP emulation software.
+ *
+ *	Revision History:
+ *
+ *	V01.000	 10-May-2017	Jonathan D. Belanger
+ *	Initially written.
+ *
+ */
+#ifndef _AXP_UTIL_DEFS_
+#define _AXP_UTIL_DEFS_
+
+#define ONE_K				1024
+#define FOUR_K				4096
+
+typedef unsigned char		u8;
+typedef unsigned short		u16;
+typedef unsigned long		u32;
+typedef unsigned long long	u64;
+typedef char				i8;
+typedef short				i16;
+typedef long				i32;
+typedef long long			i64;
+
+#endif /* _AXP_UTIL_DEFS_ */
