4c334198aaa30a74132a470d53ce8cea99841785
Jon. Belanger <jbelanger1@rochester.rr.com>
2017-11-07 12:49:20 -0500

    Changed the parameters on the one function call.

--- src/comutl/AXP_Dumps.h
+++ src/comutl/AXP_Dumps.h
@@ -31,6 +31,6 @@
 #include "AXP_Utility.h"
 #include "AXP_21264_Instructions.h"
 
-bool AXP_Decode_Instruction(AXP_INS_FMT, bool, char *);
+void AXP_Decode_Instruction(u64, AXP_INS_FMT, bool, char *);
 
 #endif	/* _AXP_DUMPS_DEFS_ */
