0b79a0b4ce5238c44e11ae5f69fa46d59fa69708
Jon. Belanger <jbelanger1@rochester.rr.com>
2017-11-17 16:44:50 -0500

    Creating a module to be called to do all the processing to create a CPU entity (memory and all the threads).

---
+++ src/cpu/AXP_21264_CPU.c
@@ -0,0 +1,28 @@
+/*
+ * Copyright (C) Jonathan D. Belanger 2017.
+ * All Rights Reserved.
+ *
+ * This software is furnished under a license and may be used and copied only
+ * in accordance with the terms of such license and with the inclusion of the
+ * above copyright notice.  This software or any other copies thereof may not
+ * be provided or otherwise made available to any other person.  No title to
+ * and ownership of the software is hereby transferred.
+ *
+ * The information in this software is subject to change without notice and
+ * should not be construed as a commitment by the author or co-authors.
+ *
+ * The author and any co-authors assume no responsibility for the use or
+ * reliability of this software.
+ *
+ * Description:
+ *
+ *	This source file contains the functions needed to create and initialize
+ *	the CPU structure and create each of the threads that implement all the
+ *	functionality of a Digital Alpha AXP 21264 CPU.
+ *
+ *	Revision History:
+ *
+ *	V01.000		17-Nov-2017	Jonathan D. Belanger
+ *	Initially written.
+ */
+#include "AXP_21264_CPUDefs.h"
