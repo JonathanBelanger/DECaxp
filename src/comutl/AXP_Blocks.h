242f533b37390eb722b5c8bc2663ded3d9dfcfef
Jon. Belanger <jbelanger1@rochester.rr.com>
2018-07-19 15:27:32 -0400

    Changed the way memory is allocated so that all allocations can use AXP_Blocks.c

--- src/comutl/AXP_Blocks.h
+++ src/comutl/AXP_Blocks.h
@@ -22,8 +22,19 @@
  *
  *	Revision History:
  *
- *	V01.000		16-May-2017	Jonathan D. Belanger
- *	Initially written.
+ *  V01.000		16-May-2017	Jonathan D. Belanger
+ *  Initially written.
+ *
+ *  V01.001		19-Jul-2018	Jonathan D. Belanger
+ *  I am changing the way memory is allocated and deallocated.  In the initial
+ *  implementation, a memory block needed to have the below structure defined
+ *  at it's top.  The new implementation will have a hidden block before and
+ *  after the block being requested.  The header and trailer blocks will have
+ *  magic numbers in them at the boundary of the memory block being requested.
+ *  This will help to determine when memory was overwritten.  Also, the header
+ *  header block will contain a place for the block to be queued up, so that we
+ *  can detect when a block is deallocated more than once, or not deallocated
+ *  at all.
  */
 #ifndef _AXP_21264_BLOCKS_
 #define _AXP_21264_BLOCKS_
@@ -38,16 +49,26 @@ typedef enum
     AXP_DISK_BLK,
     AXP_SSD_BLK,
     AXP_VHDX_BLK,
+	AXP_VOID_BLK,
     AXP_BLOCK_MAX
 } AXP_BLOCK_TYPE;
 
 typedef struct
 {
-    AXP_BLOCK_TYPE type;
-    size_t size;
-} AXP_BLOCK_DSC;
+	AXP_QUEUE_HDR	head;
+    AXP_BLOCK_TYPE	type;
+    size_t			size;
+	u64				magicNumber;
+} AXP_BLOCK_HD;
 
-void *AXP_Allocate_Block(AXP_BLOCK_TYPE blockType);
-void AXP_Deallocate_Block(AXP_BLOCK_DSC *block);
+typedef struct
+{
+	u64				magicNumber;
+	size_t			size;
+	AXP_BLOCK_TYPE	type;
+} AXP_BLOCK_TL;
+
+void *AXP_Allocate_Block(i32 blockType);
+void AXP_Deallocate_Block(void *block);
 
 #endif /* _AXP_21264_BLOCKS_ */
